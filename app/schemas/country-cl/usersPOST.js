69972ca5b5e5a6d61eeef28e0faaf0170f22800c
David Diaz <david.diaz2@walmart.com>
2023-04-22 23:13:16 -0400

    feat: update schema for user POST

--- app/schemas/country-cl/usersPOST.js
+++ app/schemas/country-cl/usersPOST.js
@@ -1,7 +1,20 @@
 const Joi = require('joi');
 
 const schema = Joi.object().keys({
-  userId: Joi.number().required()
+  rut: Joi.string().required(),
+  userName: Joi.string().required(),
+  password: Joi.string().required(),
+  email: Joi.string().required(),
+  firstName: Joi.string().required(),
+  lastName: Joi.string().required(),
+  valid: Joi.bool().required(),
+  profile: {
+    profileId: Joi.number().required(),
+    profileStatus: Joi.bool().required(),
+    profileDateInit: Joi.string().required(),
+    profileDateEnd: Joi.string().required(),
+    profileAllTime: Joi.bool().required(),
+  }
 });
 
 module.exports = {
