8a6e5390bef9862c0dfc2b3959fd1a24778e7010
David Diaz <david.diaz2@walmart.com>
2023-05-07 19:09:46 -0400

    feat: update config local and development

--- app/config/country-cl/config.development.js
+++ app/config/country-cl/config.development.js
@@ -11,7 +11,15 @@ module.exports = {
   services: {
   },
   managers: {
-    login: 'http://localhost:3100/api/manager/login'
+    login: 'http://localhost:3100/api/manager/login',
+    users: {
+      urlBase: '',
+      get: 'http://localhost:9091/api/v1/user/user-get',
+      post: 'http://localhost:9091/api/v1/user/user-add',
+      put: 'http://localhost:9091/api/v1/user/user-update',
+      delete: 'http://localhost:9091/api/v1/user/user-delete',
+      get2: 'http://jsonplaceholder.typicode.com/users',
+    }
   },
   clientPublicKey: process.env.RSA_2048_PUBLIC_CLIENT,
   clientPrivateKey: process.env.RSA_2048_PRIVATE_CLIENT
--- app/config/country-cl/config.local.js
+++ app/config/country-cl/config.local.js
@@ -14,11 +14,11 @@ module.exports = {
     login: 'http://localhost:3100/api/manager/login',
     users: {
       urlBase: '',
-      get2: 'http://jsonplaceholder.typicode.com/users',
       get: 'http://localhost:9091/api/v1/user/user-get',
       post: 'http://localhost:9091/api/v1/user/user-add',
       put: 'http://localhost:9091/api/v1/user/user-update',
-      delete: 'http://localhost:9091/api/v1/user/user-delete'
+      delete: 'http://localhost:9091/api/v1/user/user-delete',
+      get2: 'http://jsonplaceholder.typicode.com/users',
     }
   },
   clientPublicKey: process.env.RSA_2048_PUBLIC_CLIENT,
