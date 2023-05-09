6c9f60e8bee72e2d529b5d6e467d0f670a9cc6fe
David Diaz <david.diaz2@walmart.com>
2023-05-09 18:36:53 -0400

    feat: add userID in schema user POST

--- app/schemas/country-cl/usersPOST.js
+++ app/schemas/country-cl/usersPOST.js
@@ -1,6 +1,7 @@
 const Joi = require('joi');
 
 const schema = Joi.object().keys({
+  userId: Joi.string().required(),
   rut: Joi.string().required(),
   userName: Joi.string().required(),
   password: Joi.string().required(),
