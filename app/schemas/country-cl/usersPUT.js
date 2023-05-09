41f785e291262a4c36c87b0529a8ef67750f132a
David Diaz <david.diaz2@walmart.com>
2023-05-09 18:38:17 -0400

    feat: add schema for usersPUT

---
+++ app/schemas/country-cl/usersPUT.js
@@ -0,0 +1,23 @@
+const Joi = require('joi');
+
+const schema = Joi.object().keys({
+  userId: Joi.string().required(),
+  rut: Joi.string().required(),
+  userName: Joi.string().required(),
+  password: Joi.string().required(),
+  email: Joi.string().required(),
+  firstName: Joi.string().required(),
+  lastName: Joi.string().required(),
+  valid: Joi.bool().required(),
+  profile: {
+    profileId: Joi.number().required(),
+    profileStatus: Joi.bool().required(),
+    profileDateInit: Joi.string().required(),
+    profileDateEnd: Joi.string().required(),
+    profileAllTime: Joi.bool().required(),
+  }
+});
+
+module.exports = {
+  usersPUT: schema
+};
