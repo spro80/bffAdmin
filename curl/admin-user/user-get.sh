75bb5af6ed9bc2de97f6451c1d809c8ef9f63c67
David Diaz <david.diaz2@walmart.com>
2023-09-29 18:43:13 -0300

    feat: add curl for module admin-user

---
+++ curl/admin-user/user-get.sh
@@ -0,0 +1,8 @@
+curl --location --request GET 'localhost:3000/api/bff/ingress/users?userId=145157781' \
+--header 'country: CL' \
+--header 'commerce: MEDICAL' \
+--header 'channel: WEB' \
+--header 'sucursal: coquimbo' \
+--header 'usuario: 14515778' \
+--header 'traceId: 123456789' \
+--data-raw ''
